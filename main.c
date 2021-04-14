7287204ffe0eac34f36f005135c97d0b729f539e
Nicolas <nicolaszakgh@gmail.com>
2021-04-14 23:22:50 +0300

    documented all the file and changed 0Xff to 255 in getbyte(..)

--- main.c
+++ main.c
@@ -9,35 +9,53 @@ int getByte(int x, int n);
 int logicalShift(int x, int n);
 
 int main() {
-    int x = 5, y = 7;
-
-    swap(&x, &y);
     return 0;
 }
 
 void swap(int *xPointer, int *yPointer) {//swaps between to integers variables using only ^(XOR)
     printf("x = %d\n", *xPointer);
-    printf("y = %d", *yPointer);/*we know that a^a = 0 , a^0 = a for every a, and we know that ^ (Exclusive or)
+    printf("y = %d", *yPointer);
+    /*we know that a^a = 0 , a^0 = a for every a, and we know that ^ (Exclusive or)
  * has associative and Commutative properties that we will use to prove the value swap*/
-                                        // x = A , y = B
+
+    // x = A , y = B
     *xPointer = *xPointer ^ *yPointer;// x = A^B , y = B
     *yPointer = *xPointer ^ *yPointer;// x = A^B , y = (A^B)^B = A^(B^B) = A^(0) = A ,associative property
     *xPointer = *xPointer ^ *yPointer;// x = (A^B)^A = (A^A)^B = (0)^B = B , y = A ,associative + Commutative properties
-                                        // x = B , y = A
+    // x = B , y = A
 
     printf("\nx = %d\n", *xPointer);
     printf("y = %d", *yPointer);
 
 }
 
+/*
+* bitAnd ‐ x&y using only ~ and |
+* Example: bitAnd(6,5) = 4
+* Legal ops: ~ |
+* Max ops: 8 */
 int bitAnd(int x, int y) {
-    return ~(~x | ~y);
+    return ~(~x | ~y); // x&y <-> ~(~x|~y)
 }
 
+
+/*
+ *  255 is mask and is (1111 1111) in base 2 , (x >> (n << 3)) is res.
+ *  n << 3 = n*(2pow3) = n*8 converting byte to bit for shifting x's bites
+ */
 int getByte(int x, int n) {
-    return (x >> (n << 3)) & 0xff;
+    return (x >> (n << 3)) & 255;
 }
-
+/*
+ * ~(((1 << 31) >> n) << 1) is the mask
+ * (x >> n) is res
+ *
+ * (1 << 31) to get 1000 00...00 0000 31 zeros
+ * (1 << 31) >> n) means 1111 11...00 0000 (n-1) ones
+ * (((1 << 31) >> n) << 1) means 1111 10...00 0000 n ones
+ * ~(((1 << 31) >> n) << 1) means 0000 01...11 1111 n zeros
+ *  we use & operator and the mask to get rid of all the the other bits that come form the arithmetic shift
+ */
 int logicalShift(int x, int n) {
-    return (x >> n) & (~(((1 << 31) >> n) << 1));
+    return (x >> n) & ~(((1 << 31) >> n) << 1);
 }
